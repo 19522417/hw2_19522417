992fa698b23664221b56315d8a9302813be36c14
19522417 <19522417@gm.uit.edu.vn>
2022-11-14 13:58:37 +0700

    update homework2-19522417

---
+++ ex1/index.js
@@ -0,0 +1,28 @@
+function goToProfile(){
+    const element = document.getElementById("Profile");
+    element.scrollIntoView();
+}
+function goToObjective(){
+    const element = document.getElementById("Objective");
+    element.scrollIntoView();
+}
+function goToEducation(){
+    const element = document.getElementById("Education");
+    element.scrollIntoView();
+}
+function goToProject(){
+    const element = document.getElementById("skill_footer");
+    element.scrollIntoView();
+}
+function goToInterest(){
+    const element = document.getElementById("Interest");
+    element.scrollIntoView();
+}
+function goToSkill(){
+    const element = document.getElementById("Skill");
+    element.scrollIntoView();
+}
+function goToLanguages(){
+    const element = document.getElementById("Languages");
+    element.scrollIntoView();
+}
