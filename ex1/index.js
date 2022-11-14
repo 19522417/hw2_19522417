98b1693d351d39b4217a9d5f5bafd1bb021f26b3
19522417 <19522417@gm.uit.edu.vn>
2022-11-14 17:02:47 +0700

    update hw2_19522417

--- ex1/index.js
+++ ex1/index.js
@@ -1,14 +1,14 @@
 function goToProfile(){
-    const element = document.getElementById("Profile");
-    element.scrollIntoView();
+    const element1 = document.getElementById("Profile");
+    element1.scrollIntoView();
 }
 function goToObjective(){
-    const element = document.getElementById("Objective");
-    element.scrollIntoView();
+    const element2 = document.getElementById("Objective");
+    element2.scrollIntoView();
 }
 function goToEducation(){
-    const element = document.getElementById("Education");
-    element.scrollIntoView();
+    const element3 = document.getElementById("Education");
+    element3.scrollIntoView();
 }
 function goToProject(){
     const element = document.getElementById("skill_footer");
